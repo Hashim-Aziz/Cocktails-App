d739a192db0b379832169c661c29fcb851a9c3c2
HashimAziz <hashimkayani97@gmail.com>
2021-08-04 09:30:47 +0500

    setup context

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,21 @@
 import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">no cocktails match your search criteria</h2>
+    );
+  }
+
   return <h1>CocktailList </h1>;
 };
 
