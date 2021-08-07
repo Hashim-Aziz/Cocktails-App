8d2ba0fdfd5549f82e01716ec99b220d9417b674
HashimAziz <hashimkayani97@gmail.com>
2021-08-07 09:18:50 +0500

    search form functionality added

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,7 +3,35 @@ import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
-  return <h1>SearchForm </h1>;
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
